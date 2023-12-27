857913f4ae338a2626bb795eca152e0fbc13b954
shireeshavidem <shireesha.marolix@gmail.com>
2023-12-27 13:30:52 +0530

    logpage

--- Registration/App1/views.py
+++ Registration/App1/views.py
@@ -1,26 +1,44 @@
-from os import uname
-from re import U
-from django.http import HttpResponse
-from django.shortcuts import render,HttpResponse
-from django.contrib.auth.models import User
 
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required(login_url='login')
 def HomePage(request):
     return render (request,'home.html')
 
 def SignupPage(request):
     if request.method=='POST':
-        # uname=request.POST.get('username')
-        # Uemail=request.POST.get('email')
-        # upassword=request.POST.get('password')
-        # urepassword=request.POST.get('repeatPassword')
-        # my_user=User.objects.create_user(sname,semail,spassword)
-        # my_user.save()
-        return HttpResponse("User has been created sucessfully!!!")
-        print(sname,semail,spassword,srepassword)
+        uname=request.POST.get('username')
+        uemail=request.POST.get('email')
+        upassword=request.POST.get('password')
+        urepassword=request.POST.get('repeatPassword')
+
+        if upassword!=urepassword:
+            return HttpResponse("Your password and repeatpassword are not same")
+        else:
+            my_user=User.objects.create_user(uname,uemail,upassword)
+            my_user.save()
+            return redirect('login')
+        
     return render (request,'signup.html')
 
 def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("username or password is incorrect!!!")
+
     return render (request,'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
--- Registration/Registration/urls.py
+++ Registration/Registration/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.SignupPage,name='signup'),
     path('login/',views.LoginPage,name='login'),
-    path('home',views.HomePage,name='home')
+    path('home',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
 ]
